122ba611937ea3802a7f8da79c5bf00bcd5ac9f1
DESKTOP-14CE19R\Akhil <dubeysant87@gmail.com>
2017-06-05 19:44:07 +0530

    Update

--- production/content.php
+++ production/content.php
@@ -571,7 +571,7 @@ color: white;" <?php } ?>>
     
     <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-header">
+      <div class="modal-header"> 
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Search By Date Range</h4>
       </div>
